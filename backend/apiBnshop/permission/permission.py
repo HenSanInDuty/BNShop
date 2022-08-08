be3a3abc9db9953553ab396b9d3f22f7ff11531c
HenSanInDuty <heninduty@gmail.com>
2022-08-08 07:10:20 +0700

    Change some urls and db

--- backend/apiBnshop/permission/permission.py
+++ backend/apiBnshop/permission/permission.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
+#Config agency permission for user
 class AgencyPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
--- backend/apiBnshop/user/urls.py
+++ backend/apiBnshop/user/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from user.serializers import MyTokenObtainPairView
 from . import views
 
 urlpatterns = [
-    path('sign-up/', views.register, name='user-sign-up')
+    path('sign-up/', views.register, name='user-sign-up'),
+    path('sign-in/',MyTokenObtainPairView.as_view(),name='user-sign-in'),
+    path('sign-in/refresh/',TokenRefreshView.as_view(),name='user-sign-in-refresh')
 ]
