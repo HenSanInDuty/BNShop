c2c27b159cb4e7f60ddc8ff43b372abf237bbe49
= <=>
2022-11-22 21:39:18 +0700

    Config docker

--- backend/bnShopApi/bnShopApi/settings.py
+++ backend/bnShopApi/bnShopApi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,10 +93,10 @@ WSGI_APPLICATION = 'bnShopApi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'd6gtpuluvtikat', 
@@ -105,6 +106,15 @@ DATABASES = {
     #     'HOST': 'ec2-44-210-228-110.compute-1.amazonaws.com',
     #     'PORT': '5432',
     # }
+    #Docker
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bnshop",
+        "USER": "postgres",
+        "PASSWORD": "12345678",
+        "HOST": "db",
+        "PORT": "5432",
+    }
 }
 
 
