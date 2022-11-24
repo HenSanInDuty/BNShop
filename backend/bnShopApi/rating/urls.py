43e3a15c1fbc346969ae1779aa1f81efe29bace1
= <=>
2022-11-24 20:14:46 +0700

    Fix view rating

--- backend/bnShopApi/rating/urls.py
+++ backend/bnShopApi/rating/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views as rating_views
 urlpatterns = [
-    path('',rating_views.RatingViewAll.as_view(),name="rating"),
+    path('',rating_views.RatingProductViewAll.as_view(),name="rating"),
     path('<int:id>/',rating_views.RatingViewReplyDetail.as_view(),name="rating"),
     path('product-rating/',rating_views.RatingViewReplyDetail.as_view(),name="rating"),
     path('reply/<int:id>/',rating_views.ReplyViews.as_view(),name='reply')
