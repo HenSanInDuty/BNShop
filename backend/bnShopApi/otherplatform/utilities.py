31e55150e273842761c3d223b6d1fa8a6d448254
= <=>
2022-12-08 15:33:18 +0700

    Try catch in send email

--- backend/bnShopApi/otherplatform/utilities.py
+++ backend/bnShopApi/otherplatform/utilities.py
@@ -6,15 +6,18 @@ import smtplib
 from .secret import get_info_google_send_mail
 # Create your views here.
 def send_email(emailReceived:str, subject:str, content:str):
-    body = content
-    info = get_info_google_send_mail()
-    em = EmailMessage()
-    em['From'] = info['sender']
-    em['To'] = emailReceived
-    em['Subject'] = subject
-    em.set_content(body)
-    context = ssl.create_default_context()
+    try:
+        body = content
+        info = get_info_google_send_mail()
+        em = EmailMessage()
+        em['From'] = info['sender']
+        em['To'] = emailReceived
+        em['Subject'] = subject
+        em.set_content(body)
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-        smtp.login(info['sender'],info['password'])
-        smtp.send_message(em,info['sender'],emailReceived)
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            smtp.login(info['sender'],info['password'])
+            smtp.send_message(em,info['sender'],emailReceived)
+    except:
+        pass
