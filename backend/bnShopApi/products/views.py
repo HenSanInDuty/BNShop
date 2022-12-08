81dd41b464563187169575cf4fc5e70520b2acb0
= <=>
2022-12-08 16:29:38 +0700

    View product with anonymous user

--- backend/bnShopApi/products/views.py
+++ backend/bnShopApi/products/views.py
@@ -213,7 +213,7 @@ class ProductViewAll(generics.GenericAPIView):
         if request.GET.get('category'):
             category_filter = request.GET.get('category').split()
         query = Q()
-        if not request.user.is_agency:
+        if request.user and not request.user.is_agency:
             query = query.add(Q(is_approved=True,is_delete=False),Q.AND)
         if type_filter:
             sub_query = Q()
