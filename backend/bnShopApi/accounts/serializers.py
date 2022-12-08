5a6e7b20c56852cf12feb8da47b28d789c1b8bd1
= <=>
2022-12-08 07:59:17 +0700

    Fix info account

--- backend/bnShopApi/accounts/serializers.py
+++ backend/bnShopApi/accounts/serializers.py
@@ -47,7 +47,7 @@ class MyTokenObtainPairViewSerializer(TokenObtainPairSerializer):
         data['refresh'] = str(refresh)
         data['access'] = str(refresh.access_token)
         data['data'] = {
-            'id': self.user.user.id if rold != 'Admin' else self.user.id,
+            'id': self.user.user.id if role != 'Admin' else self.user.id,
             'phoneNumber': self.user.phone,
             'role': role
         }
