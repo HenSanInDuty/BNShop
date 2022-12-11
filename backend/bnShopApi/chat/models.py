7116775537216e3e18a250f098af91854f41eea6
Hen <heninduty@gmail.com>
2022-12-11 21:12:31 +0000

    Delete chat model

--- backend/bnShopApi/chat/models.py
+++ backend/bnShopApi/chat/models.py
@@ -3,10 +3,3 @@ from django.db import models
 from accounts.models import Users
 
 # Create your models here.
-class ChatRoom(models.Model):
-    user = models.ManyToManyField(Users,related_name='chat_room')
-
-class ChatLine(models.Model):
-    chat_room = models.ForeignKey(ChatRoom,on_delete=models.CASCADE,related_name='chat_line')
-    user = models.ForeignKey(Users,on_delete=models.CASCADE,related_name='chat_line')
-    content = models.TextField()
